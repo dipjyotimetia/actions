266dc04a6f6a57aa58bdbd9eeb68972c6f3b252a
Dipjyoti Metia <dipjyotimetia@gmail.com>
2021-02-13 15:09:26 +1100

    update

--- dist/index.js
+++ dist/index.js
@@ -25,7 +25,7 @@ async function run() {
 run();
 
 function calculate(payload, octokit, milestones) {
-  const hasValidMiles = validateOpenIssue(payload, octokit)
+  const hasValidMiles = validateOpenMilestone(payload, octokit)
 
   if (milestones === 'true') {
     if (hasValidMiles === false) {
@@ -34,14 +34,14 @@ function calculate(payload, octokit, milestones) {
   }
 }
 
-async function validateOpenIssue(payload, octokit) {
-  // let repoName = payload.pull_request.repository.name
+async function validateOpenMilestone(payload, octokit) {
+  let repoName = payload.repository.name
   let prNo = payload.pull_request.number
   let milestoneStatus;
   try {
     const { data: pullRequest } = await octokit.pulls.get({
       owner: 'dipjyotimetia',
-      repo: 'actions',
+      repo: repoName,
       pull_number: prNo
     })
 
